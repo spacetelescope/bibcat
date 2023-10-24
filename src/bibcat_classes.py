fe15e9852dd446ee9518b4dcd131ab1cf15fe36d
jpegues <42975929+jpegues@users.noreply.github.com>
2023-10-24 12:09:28 -0400

    Checkpoint: First pass of updates to generate_directory_TVT complete. Not run or tested yet. Updates are meant to split given dataset into TVT folders based on unique bibcodes, rather than splitting based on individual text entries (see previous commit for reason for this).

--- src/bibcat_classes.py
+++ src/bibcat_classes.py
@@ -3126,7 +3126,306 @@ class _Classifier(_Base):
 
     ##Method: generate_directory_TVT
     ##Purpose: Split a given dictionary of classified texts into directories containing training, validation, and testing datasets
-    def generate_directory_TVT(self, dir_model, fraction_TVT, mode_TVT="uniform", filename_json=None, dict_texts=None, do_shuffle=True, seed=10, do_verbose=None):
+    def generate_directory_TVT(self, dir_model, fraction_TVT, dict_texts, mode_TVT="uniform", do_shuffle=True, seed=10, do_verbose=None):
+        """
+        Method: generate_directory_TVT
+        Purpose: !!!
+        """
+        ##Load global variables
+        dataset = dict_texts
+        name_folderTVT = [preset.folders_TVT["train"],
+                        preset.folders_TVT["validate"],
+                        preset.folders_TVT["test"]]
+        #
+        num_TVT = len(name_folderTVT)
+        if (num_TVT != len(fraction_TVT)):
+            raise ValueError("Err: fraction_TVT ({0}) needs {1} fractions."
+                            .format(fraction_TVT, num_TVT))
+        #
+        if (do_verbose is None):
+            do_verbose = self._get_info("do_verbose")
+        #
+        #Set random seed, if requested
+        if do_shuffle:
+            np.random.seed(seed)
+        #
+        #Print some notes
+        if do_verbose:
+            print("\n> Running generate_directory_TVT().")
+            if do_shuffle:
+                print("Random seed set to: {0}".format(seed))
+        #
+
+        ##Load all unique paper identifiers (the bibcodes) from across texts
+        unique_bibcodes = np.unique(
+                            [dataset[key]["bibcode"] for key in dataset
+                            ]).tolist()
+        #Print some notes
+        if do_verbose:
+            print("\nDataset contains {0} papers with {1} unique bibcodes.\n"
+                    .format(len(dataset), len(unique_bibcodes)))
+        #
+
+        ##Load all unique classifs across all texts
+        count_classes = collections.Counter([dataset[key]["class"]
+                                            for key in dataset])
+        unique_classes = count_classes.keys()
+        #Print some notes
+        if do_verbose:
+            print("\nClass breakdown of given dataset:\n{0}\n"
+                    .format(count_classes))
+        #
+
+        ##Invert dataset so that classifs are stored under bibcode keys
+        dict_bibcode_classifs = {key:[] for key in unique_bibcodes}
+        dict_bibcode_textids = {key:[] for key in unique_bibcodes}
+        for curr_key in dataset:
+            curr_id = curr_key
+            curr_classif = dataset[curr_key]["class"]
+            curr_bibcode = dataset[curr_key]["bibcode"]
+            #Store id for this bibcode
+            dict_bibcode_textids[curr_bibcode].append(curr_key)
+            #Store this classif for this bibcode, if not already stored
+            if (curr_classif not in dict_bibcode_classifs[curr_bibcode]):
+                dict_bibcode_classifs.append(curr_classif) #Store classif
+            #
+        #
+        #Print some notes
+        if do_verbose:
+            print("\nGiven dataset inverted. Bibcode count: {0}"
+                    .format(len(dict_bibcode_classifs)))
+        #
+
+        ##Label bibcodes with representative classif
+        #Separate bibcodes based on number of unique associated classifs
+        all_bibcodes_oneclassif = {key1:[key2 for key2 in unique_bibcodes
+                                    if (dict_bibcode_classifs[key2] == [key1])]
+                                    for key1 in unique_classes}
+        all_bibcodes_multiclassif = [key for key in unique_bibcodes
+                                    if (len(dict_bibcode_classifs[key]) > 1)]
+        #
+        #Throw an error if separated bibcode count does not equal original count
+        tmp_check = sum(len([all_bibcodes_oneclassif[key]
+                            for key in unique_classes])
+                        + len(all_bibcodes_multiclassif))
+        if (tmp_check != len(unique_bibcodes)):
+            raise ValueError("Err: Invalid separation of bibcodes occurred.\n"
+                            +"{0} + {1} != {2}"
+                            .format(len([all_bibcodes_oneclassif[key]
+                                            for key in unique_classes]),
+                                    len(all_bibcodes_multiclassif),
+                                    len(unique_bibcodes))
+        #
+        #Shuffle the list of multi-classif bibcodes, if requested
+        if do_shuffle:
+            np.random.shuffle(all_bibcodes_multiclassif)
+        #
+        #Partition multi-classif bibcodes into representative classif lists
+        all_bibcodes_partitioned_lists = all_bibcodes_oneclassif.copy()
+        all_bibcodes_partitioned_counts = {
+                                    key:len(all_bibcodes_partitioned_lists[key])
+                                    for key in all_bibcodes_partitioned_lists
+                                    } #Starting count of bibcodes per repr.clf.
+        #Iterate through multi-classif bibcodes
+        for curr_bibcode in all_bibcodes_multiclassif:
+            curr_classifs = dict_bibcode_classifs[curr_bibcode]
+            #Determine which classif has least number of bibcodes so far
+            curr_counts = [all_bibcodes_partitioned_counts[key]
+                            for key in curr_classifs] #Current bibcode counts
+            #Store this bibcode under representative classif with min. count
+            ind_min = np.argmin(curr_counts) #Index of classif with min. count
+            min_classif = curr_classifs[ind_min]
+            all_bibcodes_partitioned_lists[min_classif
+                                            ].append(curr_bibcode) #Append bibc.
+            #Update count of this representative classif
+            all_bibcodes_partitioned_counts[min_classif] += 1
+        #
+        #Throw an error if any bibcodes are missing/duplicated in partition
+        tmp_check = sum(all_bibcodes_partitioned_counts.values())
+        if (tmp_check != len(unique_bibcodes)):
+            raise ValueError("Err: Bibcode count does not match original"
+                            +" bibcodes.\n{0}\nvs. {1}"
+                            .format(tmp_check, len(unique_bibcodes)))
+        tmp_check = [item for key in all_bibcodes_partitioned_lists
+                    for item in all_bibcodes_partitioned_lists[key]]#Part. bibc.
+        if (sorted(tmp_check) != sorted(unique_bibcodes)):
+            raise ValueError("Err: Bibcode partitioning does not match original"
+                            +" bibcodes.\n{0}\nvs. {1}"
+                            .format(tmp_check, unique_bibcodes))
+        #
+        #Print some notes
+        if do_verbose:
+            print("\nBibcode partitioning across classifications:\n{0}\n"
+                    .format(all_bibcodes_partitioned_counts))
+        #
+
+        ##Split bibcode counts into TVT sets: training, validation, testing =TVT
+        valid_modes = ["uniform", "available"]
+        fraction_TVT = np.asarray(fraction_TVT) / sum(fraction_TVT) #Normalize
+        #For mode where training sets should be uniform in size
+        if (mode_TVT.lower() == "uniform"):
+            min_count = min(all_bibcodes_partitioned_counts.values())
+            dict_split = {key:(np.round((fraction_TVT * min_count))).astype(int)
+                        for key in unique_classes}#Partition per classif per TVT
+            #Update split to send remaining bibcodes into testing datasets
+            for curr_key in unique_classes:
+                curr_max = all_bibcodes_partitioned_counts[curr_key]
+                curr_used = (dict_split[curr_key][0] + dict_split[curr_key][1])
+                dict_split[curr_key][2] = (curr_max - curr_used)
+        #
+        #For mode where training sets should use fraction of data available
+        elif (mode_TVT.lower() == "available"):
+            max_count = max(all_bibcodes_partitioned_counts.values())
+            dict_split = {key:(np.round((fraction_TVT
+                                * all_bibcodes_partitioned_counts[key]))
+                                ).astype(int)
+                        for key in unique_classes} #Partition per class per TVT
+        #
+        #Otherwise, throw error if mode not recognized
+        else:
+            raise ValueError(("Err: The given mode for generating the TVT"
+                            +" directory {0} is invalid. Valid modes are: {1}")
+                            .format(mode_TVT, valid_modes))
+        #
+        #Print some notes
+        if do_verbose:
+            print("Fractions given for TVT split: {0}\nMode requested: {1}"
+                    .format(fraction_TVT, mode_TVT))
+            print("TVT partition per class:")
+            for curr_key in unique_classes:
+                print("{0}: {1}".format(curr_key, dict_split[curr_key]))
+        #
+        #Verify splits add up to original file count
+        for curr_key in unique_classes:
+            if (all_bibcodes_partitioned_counts[curr_key]
+                                                != sum(dict_split[curr_key])):
+                raise ValueError("Err: Split did not use all data available!")
+        #
+
+        ##Prepare indices for extracting TVT sets per class
+        dict_inds = {key:[None for ii in range(0, num_TVT)]
+                    for key in unique_classes}
+        for curr_key in unique_classes:
+            #Fetch available indices
+            curr_inds = np.arange(0,all_bibcodes_partitioned_counts[curr_key],1)
+            #Shuffle, if requested
+            if do_shuffle:
+                np.random.shuffle(curr_inds)
+            #
+            #Split out the indices
+            i_start = 0 #Accumulated place within overarching index array
+            for ii in range(0, num_TVT): #Iterate through TVT
+                i_end = (i_start + dict_split[curr_key][ii]) #Ending point
+                dict_inds[curr_key][ii] = curr_inds[i_start:i_end]
+                i_start = i_end #Update latest starting place in array
+            #
+        #
+        #Print some notes
+        if do_verbose:
+            print("\nIndices split per bibcode, per TVT. Shuffling={0}."
+                    .format(do_shuffle))
+        #
+
+        ##Build new directories to hold TVT (or throw error if exists)
+        #Build model directory, if does not already exist
+        if (not os.path.exists(dir_model)):
+            os.mkdir(dir_model)
+        #
+        #Verify TVT directories do not already exist
+        if any([os.path.exists(os.path.join(dir_model, item))
+                    for item in name_folderTVT]):
+            raise ValueError("Err: TVT directories exist in model directory"
+                            +" and will not be overwritten. Please remove or"
+                            +" change the given model directory (dir_model)."
+                            +"\nCurrent dir_model: {0}".format(dir_model))
+        #
+        #Otherwise, make the directories
+        for curr_folder in name_folderTVT:
+            os.mkdir(os.path.join(dir_model, curr_folder))
+            #Iterate through classes and create subfolder per class
+            for curr_key in unique_classes:
+                os.mkdir(os.path.join(dir_model, curr_folder, curr_key))
+        #
+        #Print some notes
+        if do_verbose:
+            print("Created new directories for TVT files.\nStored in: {0}"
+                    .format(dir_model))
+        #
+
+        ##Save texts to .txt files within class directories
+        dict_info = {}
+        #Iterate through classes
+        for curr_key in unique_classes:
+            #Save each text to assigned TVT
+            for ind_TVT in range(0, num_TVT): #Iterate through TVT
+                curr_filebase = os.path.join(dir_model, name_folderTVT[ind_TVT],
+                                            curr_key) #TVT path
+                #Iterate through bibcodes assigned to this TVT
+                for ind_bibcode in dict_inds[curr_key][ind_TVT]:
+                    curr_bibcode = unique_bibcodes[ind_bibcode]
+                    #Record assigned TVT and representative classif for bibcode
+                    dict_info[curr_bibcode] = {"repr_class":curr_key,
+                                        "folder_TVT":name_folderTVT[ind_TVT]}
+                    #
+                    #Iterate through texts associated with this bibcode
+                    for curr_textid in dict_bibcodes_textids[curr_bibcode]:
+                        curr_data = dataset[curr_textid] #Data for this text
+                        curr_filename = "{0}_{1}_{2}".format("text", curr_key,
+                                                            curr_textid)
+                        #
+                        if (curr_data["id"] is not None): #Add id
+                            curr_filename += "_{0}".format(curr_data["id"])
+                        #
+                        if (curr_filename in dict_info): #Throw error if not unique
+                            raise ValueError("Err: Non-unique filename: {0}"
+                                            .format(curr_filename))
+                        #
+                        #Write this text to new file
+                        self._write_text(text=curr_data["text"],
+                                        filepath=os.path.join(curr_filebase,
+                                                        (curr_filename+".txt")))
+                #
+            #
+        #
+        #Print some notes
+        if do_verbose:
+            print("Files saved to new TVT directories.")
+        #
+
+        ##Save the dictionary of TVT bibcode partitioning to its own file
+        tmp_filesave = os.path.join(dir_model, "dict_TVTinfo.npy")
+        np.save(tmp_filesave, dict_info)
+        #Print some notes
+        if do_verbose:
+            print("Dictionary of TVT bibcode partitioning info. saved at: {0}."
+                    .format(tmp_filesave))
+        #
+
+        ##Verify that count of saved .txt files adds up to original data count
+        for curr_key in unique_classes:
+            #Count items in this class across TVT directories
+            curr_count = sum([len([item2 for item2 in
+                        os.listdir(os.path.join(dir_model, item1, curr_key))
+                        if (item2.endswith(".txt"))])
+                        for item1 in name_folderTVT])
+            #
+            #Verify count
+            if (curr_count != count_classes[curr_key]):
+                raise ValueError("Err: Unequal class count in {0}!\n{1} vs {2}"
+                        .format(curr_key, curr_count, count_classes[curr_key]))
+            #
+        #
+
+        ##Exit the method
+        if do_verbose:
+            print("\nRun of generate_directory_TVT() complete.\n---\n")
+        #
+        return
+    #
+
+    ##Method: generate_directory_TVT
+    ##Purpose: Split a given dictionary of classified texts into directories containing training, validation, and testing datasets
+    def generate_directory_TVT_old2023_10_24_beforeTVTsplitbasedonpaperoverallbibcodesinsteadofkeywordparagraphs(self, dir_model, fraction_TVT, mode_TVT="uniform", filename_json=None, dict_texts=None, do_shuffle=True, seed=10, do_verbose=None):
         """
         Method: generate_directory_TVT
         Purpose: !!!
@@ -6317,7 +6616,8 @@ class Operator(_Base):
                 #Store the modif and previous classification information
                 new_dict = {"text":curr_res["modif"],"class":old_dict["class"],
                             "id":old_dict["id"], "mission":old_dict["mission"],
-                            "forest":curr_res["forest"]}
+                            "forest":curr_res["forest"],
+                            "bibcode":old_dict["bibcode"]}
                 dict_modifs[curr_key] = new_dict
                 #
                 #Increment count of modifs generated
