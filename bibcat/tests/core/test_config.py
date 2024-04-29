a61d32862a47c1b7d0668c31e76d2cb3621e1a54
Brian Cherinka <havok2063@hotmail.com>
2024-04-29 15:34:51 -0400

    adding init config tests

---
+++ bibcat/tests/core/test_config.py
@@ -0,0 +1,37 @@
+
+import pathlib
+import pytest
+import yaml
+
+from bibcat.core.config import get_config
+
+
+def test_get_config_nouser():
+    """ test we can get the default config """
+    cc = get_config()
+    assert cc['input'] == 'a'
+    assert cc.input == 'a'
+
+
+@pytest.fixture
+def fakeyaml(tmp_path):
+    data = {'model': 'new_model'}
+    data['stuff'] = {'a': 1, 'b': 2, 'c': {'hello': 'there', 'yes': 'no'}}
+
+    path = tmp_path / "bibcat_config.yaml"
+    with open(path, 'w') as f:
+        f.write(yaml.dump(data))
+
+    yield path
+
+
+def test_user_config(monkeypatch, fakeyaml):
+    """ test we can read in a custom config """
+    monkeypatch.setenv('BIBCAT_CONFIG_DIR', str(fakeyaml.parent))
+    cc = get_config()
+    assert cc['input'] == 'a'
+    assert cc['model'] != 'b'
+    assert cc['model'] == 'new_model'
+    assert cc.stuff.c.hello == 'there'
+
+
