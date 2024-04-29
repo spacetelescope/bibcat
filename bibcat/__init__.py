0abf33e2e7c002ce2630cf97767c42f7a90f5463
Brian Cherinka <havok2063@hotmail.com>
2024-04-29 15:18:40 -0400

    initial setup of config object

--- bibcat/__init__.py
+++ bibcat/__init__.py
@@ -3,3 +3,9 @@ try:
     __version__ = _version(__name__)
 except PackageNotFoundError:
     pass
+
+# create the bibcat configuration object
+from bibcat.core.config import get_config
+config = get_config()
+
+
