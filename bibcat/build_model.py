cb6d24990af42f158c07cdb76f48defa0d75772d
jinmiyoon <jinmi.yoon@gmail.com>
2024-05-23 12:14:36 -0400

    - Checked if the local `bibcat_config.yaml` works with the default one. - Added a few print statements to log. Though we will need to use logger later instead of print statements.

--- bibcat/build_model.py
+++ bibcat/build_model.py
@@ -29,9 +29,10 @@ dir_model = os.path.join(config.paths.models, name_model)
 
 
 def build_model() -> None:
+    print(f"Starting buiding and train {dir_model}!")
     if os.path.exists(dir_model):
         print(
-            f"{dir_model} already exists. Change the model name in config.py. if you want to build and train a new model"
+            f"{dir_model} already exists. Change the model name in bibcat_config.yaml. if you want to build and train a new model"
         )
 
     # do_check_truematch: the codebase that have unknown ambiguous phrases, then a note will be
