420b43af47d6e96dc97cd47ec345c3b60f589cc3
jinmiyoon <jinmi.yoon@gmail.com>
2024-08-09 16:17:21 -0400

    combine data only when papers are in both the papertrack and papertext datasets, and fixed minor styles

--- bibcat/data/build_dataset.py
+++ bibcat/data/build_dataset.py
@@ -22,30 +22,32 @@ logger = setup_logger(__name__)
 
 
 def file_exists(filelist: list) -> bool:
+    "Check if any file exists among the list of files"
     return any([os.path.isfile(item) for item in filelist])
 
 
 def save_text_file(path_filename: Path, bibcodes: list[str]) -> None:
-    np.savetxt(
-        path_filename,
-        np.array(bibcodes),
-        delimiter="\n",
-        fmt="%s",
-    )
+    try:
+        with path_filename.open(mode="w") as file:
+            file.write("\n".join(bibcodes))
+    except IOError as e:
+        print(f"An error occurred while saving the file: {e}")
 
 
 def load_datasets(path_papertext: Path, path_papertrack: Path) -> tuple[list[dict], list[dict]]:
     # Load paper texts and papertrack classes
+    logger.info("Loading papertext and papertrack datasets!")
     papertext_dataset = load_json_file(path_papertext)
     papertrack_dataset = load_json_file(path_papertrack)
+    logger.info("Loaded papertext and papertrack datasets!")
     return papertext_dataset, papertrack_dataset
 
 
 def extract_papertext_info(dataset) -> tuple[list[str], list[str]]:
     bibcodes = [entry["bibcode"] for entry in dataset]
     pubdates = [entry["pubdate"] for entry in dataset]
-    logger.debug(f"The earliest date of papers within text database: {min(pubdates)}.")
-    logger.debug(f"The latest date of papers within text database: {max(pubdates)}.")
+    logger.info(f"The earliest date of papers within text database: {min(pubdates)}.")
+    logger.info(f"The latest date of papers within text database: {max(pubdates)}.")
 
     return bibcodes, pubdates
 
@@ -69,56 +71,61 @@ def missing_bibcodes_in_papertext(
     # Verify that all papers within papertrack are within the papertext database
     bibcodes_notin_papertext = [val for val in np.unique(bibcodes_papertrack) if (val not in bibcodes_papertext)]
     if len(bibcodes_notin_papertext) > 0:
-        errstr = (
+        logger.warning(
             "Note! Papers in papertrack not in text database!"
             + f"\n{bibcodes_notin_papertext}\n{len(bibcodes_notin_papertext)} of {len(bibcodes_papertrack)} papertrack entries in all.\n"
         )
-        logger.error(errstr)
     return bibcodes_notin_papertext
 
 
-def trim_papertext_dict(dataset: dict, keys: list) -> list[dict]:
-    storage_combined_dataset = [
-        {key: value for key, value in thisdict.items() if (key in keys)}.copy() for thisdict in dataset
-    ]
-    return storage_combined_dataset
+def trim_dict(dataset: dict, keys: list) -> list[dict]:
+    logger.info(f"trimming the papertext dict with {keys}")
+    logger.debug(f"the first entry of the loaded_papertext = {dataset[0]}")
+
+    trimmed_dict = [{key: value for key, value in thisdict.items() if (key in keys)}.copy() for thisdict in dataset]
+    logger.debug(f"Show the first entry of the trimmed data: \n {trimmed_dict[0]}")
+    logger.debug(f"Dict trimming is complete.")
+
+    return trimmed_dict
 
 
-def combine_datasets(papertext_data, papertrack_data):
-    # First, store trimmed papertext dictionary down to only columns to include
-    data_storage = trim_papertext_dict(papertext_data, config.inputs.keys_papertext)
-
+def combine_datasets(trimmed_papertext_data: list[dict], papertrack_data: list[dict]):
+    logger.info("Start combining the two datasets.")
     # Extract information from the papertrack classification dataset
     ads_searches, bibcodes_papertrack, missions_and_papertypes = extract_papertrack_info(papertrack_data)
 
     # Extract information from the paper text dataset
-    bibcodes_papertext, _ = extract_papertext_info(papertext_data)
+    bibcodes_papertext, _ = extract_papertext_info(trimmed_papertext_data)
 
     # Verify that all papers within papertrack are within the papertext database and return the bibcodes
     bibcodes_notin_papertext = missing_bibcodes_in_papertext(bibcodes_papertext, bibcodes_papertrack)
 
     bibcodes_notin_papertrack = []
-
-    for curr_index, curr_dict in enumerate(data_storage):
+    combined_dataset = []
+    new_dict = {}
+    for _, curr_dict in enumerate(trimmed_papertext_data):
         # Extract information for current paper within text database
         curr_bibcode = curr_dict["bibcode"]
 
         if curr_bibcode in bibcodes_papertrack:
             index = bibcodes_papertrack.index(curr_bibcode)
-            curr_dict["class_missions"] = {
+            new_dict["class_missions"] = {
                 mission["mission"]: {"bibcode": curr_bibcode, "papertype": mission["paper_type"]}
                 for mission in missions_and_papertypes[index]
             }
+
             for search in ads_searches[index]:
-                curr_dict[f"is_ignored_{search['search_key']}"] = search["ignored"]
+                new_dict[f"is_ignored_{search['search_key']}"] = search["ignored"]
+
+            combined_dataset.append({**curr_dict, **new_dict})
         else:
-            logger.debug(f"Bibcode ({curr_dict['bibcode']}) not in papertrack database. Continuing...")
+            logger.warning(f"Bibcode ({curr_dict['bibcode']}) not in papertrack database. Continuing...")
             bibcodes_notin_papertrack.append(curr_bibcode)
     logger.info(f"NOTE: {len(bibcodes_notin_papertrack)} papers in text data that were not in papertrack.")
     logger.info("Done generating dictionaries of combined papertrack+text data.")
 
     return (
-        data_storage,
+        combined_dataset,
         bibcodes_notin_papertext,
         bibcodes_notin_papertrack,
     )
@@ -140,30 +147,17 @@ def save_files(dataset: dict, missing_papertext_bibcodes: list, missing_papertra
 def build_dataset() -> None:
     logger.info("The script is building the dataset for bibcat!")
 
-    # Throw an error if any of these files already exist
-    tmp_list = [
-        config.inputs.path_source_data,
-        config.output.path_not_in_papertext,
-        config.output.path_not_in_papertrack,
-    ]
-    if file_exists(tmp_list):
-        logger.error(
-            "Err: File in the following list already exists and will not be overwritten."
-            + f"\nPlease change the save destination for the notebook file(s) or move the existing files.\n{tmp_list}"
-        )
-        return
-
-    else:
-        # Load paper texts and papertrack classes
-        dataset_papertext_orig, dataset_papertrack_orig = load_datasets(
-            config.inputs.path_papertext, config.inputs.path_papertrack
-        )
-        # combine the papertrack and papertext into one dataset
-        storage_combined_dataset, bibcodes_notin_papertext, bibcodes_notin_papertrack = combine_datasets(
-            dataset_papertext_orig,
-            dataset_papertrack_orig,
-        )
-
-        # Save the combined dataset
-        save_files(storage_combined_dataset, bibcodes_notin_papertext, bibcodes_notin_papertrack)
-        save_files(storage_combined_dataset, bibcodes_notin_papertext, bibcodes_notin_papertrack)
+    # Load paper texts and papertrack classes
+    dataset_papertext_orig, dataset_papertrack_orig = load_datasets(
+        config.inputs.path_papertext, config.inputs.path_papertrack
+    )
+    # First, store trimmed papertext dictionary down to only columns to include
+    trimmed_papertext_dataset = trim_dict(dataset_papertext_orig, config.inputs.keys_papertext)
+    # combine the papertrack and papertext into one dataset
+    combined_dataset, bibcodes_notin_papertext, bibcodes_notin_papertrack = combine_datasets(
+        trimmed_papertext_dataset,
+        dataset_papertrack_orig,
+    )
+    # Save the combined dataset
+    save_files(combined_dataset, bibcodes_notin_papertext, bibcodes_notin_papertrack)
+    logger.info("The combined dataset is created as well as bibcodes_notin_papertext and bibcodes_notin_papertrack!")
