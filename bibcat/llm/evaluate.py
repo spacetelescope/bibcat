66b8b5b019317276c2f8547d76998641d61039cc
jinmiyoon <jinmi.yoon@gmail.com>
2024-09-05 15:50:34 -0400

    Include title and abstract in the text to be evaluated to identify_mission_in_text.

--- bibcat/llm/evaluate.py
+++ bibcat/llm/evaluate.py
@@ -11,7 +11,7 @@ from bibcat.llm.io import get_source, read_output, write_summary
 from bibcat.utils.logger_config import setup_logger
 
 # set up global operator
-op = Operator(classifier='ML', mode=None, keyword_objs=params.all_kobjs)
+op = Operator(classifier="ML", mode=None, keyword_objs=params.all_kobjs)
 
 # set up logger
 logger = setup_logger(__name__)
@@ -19,7 +19,7 @@ logger.setLevel(config.logging.level)
 
 
 def evaluate_output(bibcode: str = None, index: int = None, threshold: float = 0.5) -> pd.DataFrame:
-    """ Evaluate the output from the LLM model
+    """Evaluate the output from the LLM model
 
     For a given paper bibcode, reads in the output from the LLM model and
     evaluates its performance against the human paper classifications. It matches
@@ -50,59 +50,83 @@ def evaluate_output(bibcode: str = None, index: int = None, threshold: float = 0
     """
     # get the output
     paper = get_source(bibcode=bibcode, index=index)
-    bibcode = paper['bibcode']
+    bibcode = paper["bibcode"]
     response = read_output(bibcode)
 
     # exit if no bibcode found in output
     if not response:
-        logger.info(f'No output found for {bibcode}')
+        logger.info(f"No output found for {bibcode}")
         return None
 
     n_runs = len(response)
 
-    logger.info(f'Evaluating output for {bibcode}')
-    logger.info(f'Number of runs: {n_runs}')
+    logger.info(f"Evaluating output for {bibcode}")
+    logger.info(f"Number of runs: {n_runs}")
 
     # convert output to a dataframe
-    df = pd.DataFrame([(k, v[0], v[1]) for i in response for k, v in i.items()],
-                      columns=['mission', 'papertype', 'llm_confidence'])
-    df = df.sort_values('mission').reset_index(drop=True)
+    df = pd.DataFrame(
+        [(k, v[0], v[1]) for i in response for k, v in i.items()], columns=["mission", "papertype", "llm_confidence"]
+    )
+    df = df.sort_values("mission").reset_index(drop=True)
 
     # group by mission and paper type,
     # get the mean confidence and the count in each group
-    grouped_df = df.groupby(['mission', 'papertype']).\
-        agg(mean_llm_confidence=('llm_confidence', 'mean'), std_llm_confidence=('llm_confidence', 'std'), count=('llm_confidence', 'size')).reset_index().\
-            rename(columns={'mission':'llm_mission', 'papertype':'llm_papertype'})
-    grouped_df['n_runs'] = n_runs
+    grouped_df = (
+        df.groupby(["mission", "papertype"])
+        .agg(
+            mean_llm_confidence=("llm_confidence", "mean"),
+            std_llm_confidence=("llm_confidence", "std"),
+            count=("llm_confidence", "size"),
+        )
+        .reset_index()
+        .rename(columns={"mission": "llm_mission", "papertype": "llm_papertype"})
+    )
+    grouped_df["n_runs"] = n_runs
 
     # get the human paper classifications
-    human_classes = paper['class_missions']
+    human_classes = paper["class_missions"]
     formatted_output = "\n".join([f"{mission}: {info['papertype']}" for mission, info in human_classes.items()])
     logger.info(f"Human Classifications:\n {formatted_output}")
 
     # compute consistency of matches to human classification
-    vv = [(k, v['papertype']) for k, v in human_classes.items()]
-    grouped_df['consistency'] = grouped_df.apply(lambda x: (x['count']/x['n_runs']) * 100 if (x['llm_mission'], x['llm_papertype']) in vv else 0, axis=1)
-    grouped_df['in_human_class'] = grouped_df.apply(lambda x: (x['llm_mission'], x['llm_papertype']) in vv, axis=1)
+    vv = [(k, v["papertype"]) for k, v in human_classes.items()]
+    grouped_df["consistency"] = grouped_df.apply(
+        lambda x: (x["count"] / x["n_runs"]) * 100 if (x["llm_mission"], x["llm_papertype"]) in vv else 0, axis=1
+    )
+    grouped_df["in_human_class"] = grouped_df.apply(lambda x: (x["llm_mission"], x["llm_papertype"]) in vv, axis=1)
 
     # get missing missions
-    missing_by_human = set(grouped_df['llm_mission']) - set(human_classes)
-    missing_by_llm = set(human_classes)-set(grouped_df['llm_mission'])
+    missing_by_human = set(grouped_df["llm_mission"]) - set(human_classes)
+    missing_by_llm = set(human_classes) - set(grouped_df["llm_mission"])
 
     # check if missions are in the paper text body
-    in_text = identify_missions_in_text(grouped_df['llm_mission'], paper['body'])
-    grouped_df['mission_in_text'] = in_text
+    in_text = identify_missions_in_text(
+        grouped_df["llm_mission"],
+        "Title: " + paper["title"] + "\nAbstract: " + paper["abstract"] + "\nFull Text: " + paper["body"],
+    )
+    grouped_df["mission_in_text"] = in_text
 
     # log the output
     logger.info("Output Stats by LLM Mission and Paper Type:\n" + grouped_df.to_string(index=False))
-    logger.info('Missing missions by humans: ' + ', '.join(missing_by_human))
-    logger.info('Missing missions by LLM: ' + ', '.join(missing_by_llm))
+    logger.info("Missing missions by humans: " + ", ".join(missing_by_human))
+    logger.info("Missing missions by LLM: " + ", ".join(missing_by_llm))
 
     # write the summary output
-    llm = [{i['llm_mission']: i['llm_papertype']} for i in grouped_df.to_dict(orient='records') if i['mean_llm_confidence'] >= threshold]
-    output = {bibcode: {'human': {k:v['papertype'] for k, v in human_classes.items()}, 'threshold': threshold, 'llm': llm,
-              'missing_by_human': list(missing_by_human), 'missing_by_llm': list(missing_by_llm),
-              'df': grouped_df.to_dict(orient='records')}}
+    llm = [
+        {i["llm_mission"]: i["llm_papertype"]}
+        for i in grouped_df.to_dict(orient="records")
+        if i["mean_llm_confidence"] >= threshold
+    ]
+    output = {
+        bibcode: {
+            "human": {k: v["papertype"] for k, v in human_classes.items()},
+            "threshold": threshold,
+            "llm": llm,
+            "missing_by_human": list(missing_by_human),
+            "missing_by_llm": list(missing_by_llm),
+            "df": grouped_df.to_dict(orient="records"),
+        }
+    }
     write_summary(output)
 
     # return the dataframe
@@ -110,9 +134,9 @@ def evaluate_output(bibcode: str = None, index: int = None, threshold: float = 0
 
 
 def identify_missions_in_text(missions: list, text: str) -> list:
-    """ Check if a mission is in the paper text body
+    """Check if a mission is in the paper text
 
-    Checks if a list of mission names are present in the body of
+    Checks if a list of mission names are present in the title, abstract, and body of
     the paper text.  The text comes from the "body" field of the
     source dataset.  First, it loads the text into the bibcat Paper object,
     parses, and retrieves the paragraphs matching all the bibcat mission
@@ -140,7 +164,6 @@ def identify_missions_in_text(missions: list, text: str) -> list:
 
     in_text = []
     for mission in missions:
-
         # get the relevant mission keyword
         keyword = op._fetch_keyword_object(mission)
 
@@ -148,5 +171,3 @@ def identify_missions_in_text(missions: list, text: str) -> list:
         in_text.append(True if paragraphs.get(keyword.get_name()) else False)
 
     return in_text
-
-
