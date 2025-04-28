a459cc48216ffe90ef566f3c57c71230bfc36e8d
Dan Burger <dburger@stsci.edu>
2025-04-28 16:38:18 -0400

    _fetch_keyword_object

--- bibcat/llm/evaluate.py
+++ bibcat/llm/evaluate.py
@@ -422,3 +422,54 @@ def identify_missions_in_text(missions: list, text: str) -> list:
         in_text.append(True if paragraphs.get(keywd) else False)
 
     return in_text
+
+    # Fetch a keyword object that matches the given lookup
+    def _fetch_keyword_object(self, lookup: str, do_raise_emptyerror: bool = True) -> Any | None:
+        """Fetch a keyword object
+
+        Given an input lookup string, tries to match it to a stored Keyword instance.
+
+        Parameters
+        ----------
+        lookup : str
+            the input string
+        do_raise_emptyerror : bool, optional
+            Flag to raise an error, by default True
+
+        Returns
+        -------
+        Any | None
+            the matching Keyword instance
+
+        Raises
+        ------
+        ValueError
+            when no match is found
+        """
+
+        # Print some notes
+        if self.verbose:
+            logger.info(f"> Running _fetch_keyword_object() for lookup term {lookup}.")
+
+        # Find keyword object that matches to given lookup term
+        match = None
+        for kobj in self.keyword_objs:
+            # If current keyword object matches, record and stop loop
+            if kobj.identify_keyword(lookup)["bool"]:
+                match = kobj
+                break
+
+        # Throw error if no matching keyword object found
+        if match is None:
+            errstr = f"No matching keyword object for {lookup}.\n"
+            errstr += "Available keyword objects are:\n"
+            # just use the names of the keywords
+            names = ", ".join(a._get_info("name") for a in self.keyword_objs)
+            errstr += f"{names}\n"
+
+            # Raise error if so requested
+            if do_raise_emptyerror:
+                raise ValueError(errstr)
+
+        # Return the matching keyword object
+        return match
--- bibcat/tests/llm/test_eval.py
+++ bibcat/tests/llm/test_eval.py
@@ -1,6 +1,9 @@
 import pytest  # noqa: F401
 
-from bibcat.llm.evaluate import evaluate_output, group_by_mission
+from bibcat.core import parameters as params
+from bibcat.core.operator import Operator
+from bibcat.llm.evaluate import _fetch_keyword_object, evaluate_output, group_by_mission
+from bibcat.tests.core.test_config import test_dict_lookup_kobj
 
 paper = {
     "bibcode": "2022Sci...377.1211L",
@@ -166,3 +169,48 @@ def test_group_by_mission(mocker):
     assert mm.iloc[1]["total_weighted_conf"].tolist() == [0.8, 0.2]
     assert mm.iloc[1]["prob_mission"] == 0.667
     assert mm.iloc[1]["prob_papertype"].tolist() == [0.8, 0.2]
+
+
+def test__fetch_keyword_object__overlap(self):
+    # Prepare text and answers for test
+    tmp_kobj_list = [params.kobj_hubble, params.kobj_hla]
+    dict_acts = {
+        "Hubble Legacy Archive": "HLA",
+        "Hubble Legacy Archive results": "HLA",
+        "Hubble": "Hubble",
+        "HST": "Hubble",
+        "HLA": "HLA",
+        "Hubble Archive": "Hubble",
+        "Hubble Legacy": "Hubble",
+    }
+
+    # Prepare and run test for bibcat class instance
+    """testbase = Operator(
+        classifier=None,
+        mode=None,
+        keyword_objs=tmp_kobj_list,
+        verbose=False,
+        name="operator",
+        load_check_truematch=False,
+        deep_verbose=False,
+    )"""
+    # Determine and check answers
+    for key1 in dict_acts:
+        # Otherwise, check generated modif
+        curr_lookup = key1
+        test_res = _fetch_keyword_object(lookup=curr_lookup, do_raise_emptyerror=True)
+        curr_answer = test_dict_lookup_kobj[dict_acts[key1]]
+
+        # Check answer
+        try:
+            self.assertEqual(test_res, curr_answer)
+        except AssertionError:
+            print("")
+            print(">")
+            print(("Text: {2}\n\nTest answer: {0}\n" + "\nAct. answer: {1}\n").format(test_res, curr_answer, key1))
+            print("---")
+            print("")
+
+            self.assertEqual(test_res, curr_answer)
+
+            self.assertEqual(test_res, curr_answer)
