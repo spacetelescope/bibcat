a9e86aa976d365098f8a12b6c66c6d6f6ebac58c
Dan Burger <dburger@stsci.edu>
2025-04-28 16:47:25 -0400

    saved files

--- bibcat/tests/core/test_operator.py
+++ bibcat/tests/core/test_operator.py
@@ -6,11 +6,6 @@ Testing the Operator class and its methods.
 
 import unittest
 
-from test_config import test_dict_lookup_kobj
-
-from bibcat.core import parameters as params
-from bibcat.core.operator import Operator
-
 
 # Purpose: Testing the Operator class
 class TestOperator(unittest.TestCase):
