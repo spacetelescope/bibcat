e36ad0ac8aaf0c88218dc99fc63e5741dd595093
jinmiyoon <jinmi.yoon@gmail.com>
2025-05-13 10:36:35 -0400

    fix TypeError for JSON output

--- bibcat/utils/utils.py
+++ bibcat/utils/utils.py
@@ -51,4 +51,9 @@ class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
-        return super(NumpyEncoder, self).default(obj)
+        elif isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        else:
+            return super(NumpyEncoder, self).default(obj)
