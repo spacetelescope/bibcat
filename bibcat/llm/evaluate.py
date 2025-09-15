ae451c4874741ad149e3d17e405237d3df66d1da
Jinmi Yoon <jyoon@stsci.edu>
2025-09-15 17:05:24 -0400

    fix KeyError:nan due to getting two papertypes for the same mission;  update grouped_df to drop NaN mission or keep the papertype which has the larger weighted_confs value between the two types

--- bibcat/llm/evaluate.py
+++ bibcat/llm/evaluate.py
@@ -344,6 +344,15 @@ def prepare_output(
     # reindex mission
     mm = mission_df.set_index("llm_mission")
 
+    # drop NaN mission and keep only the papertype which has the largest value in weighted_confs list
+    # This is to prevent halting evaluate run when a mission have both the papertypes
+    grouped_df = (
+        grouped_df.dropna(subset=["llm_mission"])
+        .groupby("llm_mission", group_keys=False)
+        .apply(lambda g: g.loc[g["weighted_confs"].apply(max).idxmax()])
+        .reset_index(drop=True)
+    )
+
     # pass its llm's classification if the maximum weighted-confidence value is higher than the threshold
     # the maximum value is used because the papertype's confidence is aligned with the maximum value
     llm = [
