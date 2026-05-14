2a38352ebb6f5f8bfef311e4371bb0f03d8c8dbe
Dan Burger <dburger@stsci.edu>
2026-05-14 11:11:45 -0400

    ASB-31862 only log debug messages if level is set to DEBUG

--- bibcat/utils/logger_config.py
+++ bibcat/utils/logger_config.py
@@ -15,7 +15,6 @@ os.makedirs(LOG_DIR, exist_ok=True)  # Ensure the logs directory exists
 
 def setup_logger(name: str, level: int = logging.DEBUG) -> logging.Logger:
     """Set up logger"""
-    # Create a custom logger
     logger = logging.getLogger(name)
     if logger.hasHandlers():
         return logger
@@ -27,8 +26,11 @@ def setup_logger(name: str, level: int = logging.DEBUG) -> logging.Logger:
     # Create handlers
     c_handler = logging.StreamHandler()
     f_handler = logging.FileHandler(log_file_path)
-    c_handler.setLevel(level)
-    f_handler.setLevel(level)
+
+    # Only log DEBUG messages if level is explicitly set to DEBUG
+    handler_level = logging.DEBUG if level == logging.DEBUG else logging.INFO
+    c_handler.setLevel(handler_level)
+    f_handler.setLevel(handler_level)
 
     # Create formatters and add it to handlers
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
