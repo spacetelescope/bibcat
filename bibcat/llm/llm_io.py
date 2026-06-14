79bf88e19107ce4241b81fd3a08695196f131cd7
Jinmi Yoon <jyoon@stsci.edu>
2026-06-14 17:16:38 -0400

    minor update

--- bibcat/llm/llm_io.py
+++ bibcat/llm/llm_io.py
@@ -180,7 +180,7 @@ def write_output(paper_key: str, response: dict):
             content = f.read()
             data = json.loads(content) if content.strip() else {}
 
-        # append response to an existing file entry, or add a new one with a new paper_key or in the OPS mode
+        # append response to an existing file entry, or add a new one with a new paper_key
         if paper_key in data:
             # logger.info(f"Appending the new run to {paper_key}")
             data[paper_key].append(response)
